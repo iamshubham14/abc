bad2a4fff4ef9441a4b881a63632565125254725
iamshubham14 <43136157+iamshubham14@users.noreply.github.com>
2019-02-06 17:32:10 +0530

    huuu

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <table>
+      <?php
+
+      echo "shubham"; ?>
+        <base href="" target="_blank">
+<img src="" alt="">
+
+<input type="number" name="" value="">
+<script type="text/javascript">
+</script>    </table>
+  </body>
+</html>
